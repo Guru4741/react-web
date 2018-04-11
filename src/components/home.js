205a3cb98178f6b79dc910c2afa4c5ced635eff3
Guru4741 <gaurav4741@gmail.com>
2018-04-11 16:43:17 +0530

    Initial commit

---
+++ src/components/home.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import {Jumbotron, Grid, Row, Col, Image, Button} from 'react-bootstrap';
+import './home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Grid>
+        <Jumbotron>
+          <h2>Welcome to CodeLife</h2>
+          <p>This is how to build a website with react, react-bootstrap and react-router-dom.</p>
+          <Link to = '/about'>
+          <Button bsStyle = "primary">About</Button>
+        </Link>
+        </Jumbotron>      
+        <Row className="show-grid text-center">
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="assets/person-1.jpg" circle className="profile-pic"/>
+            <h3>Frank</h3>
+            <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
+          </Col>
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="assets/person-2.jpg" circle className="profile-pic"/>
+            <h3>Vanessa</h3>
+            <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
+          </Col>
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="assets/person-3.jpg" circle className="profile-pic"/>
+            <h3>Riff</h3>
+            <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
+
+
